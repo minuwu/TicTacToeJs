3be4d5636a5210db4335d52cfd8cd67f794f35a0
minuwu <84508269+minuwu@users.noreply.github.com>
2024-02-04 09:37:18 +0600

    initial logic

---
+++ app.js
@@ -0,0 +1,78 @@
+let msg = document.querySelector(".msg");
+msg.innerText = "TIC TAC TOE";
+let reset = document.querySelector(".reset");
+let turn = true;
+let winSequence = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let cnt=0;
+for(let i=1; i<=9; i++){    
+    let box = document.createElement("button");
+    box.classList.add("box");
+    document.querySelector(".gameContainer").append(box);
+    box.addEventListener("click",()=>{
+        cnt++;
+        if(turn){
+            box.innerText = "X";
+            turn = false;
+        }else{
+            box.innerText = "O";
+            turn = true;
+        }
+        win();
+        ded(box);
+        if(cnt==9){
+            msg.innerText = "Game Draw!";
+            cnt = 0;
+        }
+    })
+}
+
+let boxes = document.querySelectorAll(".box");
+
+
+function win(){
+    for(let winner of winSequence){
+        let
+        a = winner[0],
+        b = winner[1],
+        c = winner[2];
+
+        if( boxes[a].innerText && boxes[b].innerText && boxes[c].innerText){
+            if(boxes[a].innerText == boxes[b].innerText && boxes[b].innerText==boxes[c].innerText){
+                console.log("winner found");
+                for(let box of boxes){
+                    ded(box);
+                }
+                msg.innerText = `Winner is ${boxes[a].innerText}`;
+
+            }
+        }
+    }
+}
+function ded(box){
+    box.disabled = true;
+    box.style.backgroundColor = "#c7dad4";
+}
+function alive(box){
+    box.disabled = false;
+    box.style.backgroundColor = "#E5FCF5"
+}
+reset.addEventListener("click",() =>{
+    for(let box of boxes){
+        box.innerText = "";
+        alive(box);
+    }
+    turn = true;
+    msg.innerText = "Tic Tac Toe";
+    cnt = 0;
+})
+
+
